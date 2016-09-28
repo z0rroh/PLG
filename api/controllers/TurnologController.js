ff2fec04d1d5c3db6dc694b853c2c00230eb291f
Zorro <caarlos@live.cl>
2016-09-28 11:24:29 -0300

    post y get turnolog

--- api/controllers/TurnologController.js
+++ api/controllers/TurnologController.js
@@ -9,41 +9,51 @@ module.exports = {
 
   entrar:function (req,res) {
 
-
-      //if(req.method === 'POST') {
+      if(req.method === 'POST') {
         Turnolog.findOne(req.param('id'))
                 .then(function(result){
+
                   var turnolog = result;
-                  if (turnolog.estado===true && turnolog.cupoActual<turnolog.cupoTotal){
-                    if(turnolog.users){
-                      for(var i in turnolog.users){
-                        if(turnolog.users[i].id == req.session.User.id){
-                          console.log('return false');
-                          break;
-                        }
-                      }
-                      if(req.session.User.tokens >0){
-                        req.session.User.tokens = req.session.User.tokens -1;
-                        var userRecord = req.session.User;
-                        userRecord.save();
+                  if(turnolog.users){
+                  var resul= false;
+                  for(var i in turnolog.users){
+                    if(turnolog.users[i].id == req.session.User.id){
+                      resul=true;
+                    }
+                  }
+                  console.log(resul);
+
+                  if (resul===false && turnolog.estado===true && turnolog.cupoActual<turnolog.cupoTotal && req.session.User.tokens>0){
                         var userObj = {
                           id: req.session.User.id,
                           name: req.session.User.name
                         };
-                        turnolog.users = [];
+                        var actual = turnolog.cupoActual;
+                        var parsed = parseInt(actual, 10);
+                        parsed = parsed + 1;
+                        turnolog.cupoActual = parsed;
+                        console.log(req.session.User.tokens);
                         turnolog.users.push(userObj);
                         turnolog.save(function(err){
                             req.session.flash={
                               err:err
                             }
                         });
-                      }
+                        req.session.User.tokens = req.session.User.tokens-1;
+                        console.log(req.session.User.tokens);
                     }
+                  }
+
                     else{
+                      console.log('hello');
                       var userObj = {
                         id: req.session.User.id,
                         name: req.session.User.name
                       };
+                      var actual = turnolog.cupoActual;
+                      var parsed = parseInt(actual, 10);
+                      parsed = parsed + 1;
+                      turnolog.cupoActual = parsed;
                       turnolog.users = [];
                       turnolog.users.push(userObj);
                       turnolog.save(function(err){
@@ -51,26 +61,8 @@ module.exports = {
                             err:err
                           }
                       });
+                      req.session.User.tokens = req.session.User.tokens-1;
                     }
-                    /*
-                    if(reformattedArray == true){
-                      //turnolog.cupoActual = cupoActual++;
-                      //req.session.User.tokens = req.session.User.tokens-1;
-                      turnolog.users.push({
-                        id: req.session.User.id,
-                        name: req.session.User.name
-                      });
-                      console.log(turnolog);
-                      turnolog.save(
-                      function(err){
-                        req.session.flash={
-                          err:err
-                        }
-
-                      });
-                    }*/
-                  }
-
         })
         .fail(function(err){
           req.session.flash={
@@ -78,27 +70,27 @@ module.exports = {
           }
 
         });
-
-      //}
+        res.redirect('/anuncios/index');
+      }
       /*
       else if(req.isSocket){
         Chat.watch(req.socket);
         sails.log( 'User subscribed to ' + req.socket.id );
       }
-
+*/
       if(req.method === 'GET') {
-        Turnolog.find({group:req.session.User.id_group},function(err,turnos){
+        Turnolog.find({group:req.session.User.id_group},function(err,turnologs){
           if(err) {
             sails.log(err);
             sails.log("Error occurred in database operation");
           }
           else {
-            console.log(turnos);
-            res.send(turnos);
+            console.log(turnologs);
+            res.send(turnologs);
           }
         });
       }
-      */
+
   },
 
 
